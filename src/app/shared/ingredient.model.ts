389d274833114de0d18af1873a7cf6550e6ab509
Tyrone <tyrone.saulon@hotmail.com>
2018-08-07 01:44:08 -0400

    Implementing ingredient list component

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,6 @@
+export class Ingredient{
+
+    //  declares names in the parameter list as a shortcut.
+    //  no need to declare member variables outside of constructor.
+    constructor(public name: string, public amount: number){}
+}
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListComponent implements OnInit {
 
-  constructor() { }
+  ingredients: Ingredient[];
+
+  constructor() { 
+    this.ingredients = [
+      new Ingredient("Apple", 5),
+      new Ingredient("Potato", 500)
+    ]
+  }
 
   ngOnInit() {
   }
