9262290b2bf68e7b3790b1ffbec7b7f2f786277e
Tyrone <tyrone.saulon@hotmail.com>
2018-09-08 09:55:49 -0400

    Refactoring communication between recipe-list and recipe-item

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
+
 export class ShoppingEditComponent implements OnInit {
+
+  @Output() requestAdd = new EventEmitter<{name: string, amount: number}>();
+  @Output() requestClear = new EventEmitter();
+  @Output() requestDelete = new EventEmitter<string>();
+
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  requestAddItem(itemName: HTMLInputElement, itemAmount: HTMLInputElement) {
+    this.requestAdd.emit({name: itemName.value, amount: <number><any>itemAmount.value});
+  }
+
+  requestClearItemList() {
+    this.requestClear.emit();
+  }
+
+  requestDeleteItem(itemName: HTMLInputElement) {
+    this.requestDelete.emit(itemName.value);
   }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -10,14 +10,37 @@ export class ShoppingListComponent implements OnInit {
 
   ingredients: Ingredient[];
 
-  constructor() { 
+  constructor() {
     this.ingredients = [
-      new Ingredient("Apple", 5),
-      new Ingredient("Potato", 500)
-    ]
+      new Ingredient('Beef', 5),
+      new Ingredient('Collard Greens', 3)
+    ];
   }
 
   ngOnInit() {
+
+  }
+
+  addItem({name, amount}) {
+    this.ingredients.push(new Ingredient(name, amount));
+  }
+
+  clearItemList() {
+    this.ingredients = [];
+  }
+
+  deleteItem(name) {
+
+    let flag = -1;
+
+    this.ingredients.forEach((item, index) => {
+      if (name.toUpperCase() ===  item.name.toUpperCase()) {
+        flag = index;
+      }
+    });
+
+    this.ingredients.splice(flag, 1);
+
   }
 
 }
